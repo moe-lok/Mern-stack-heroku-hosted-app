15fadb3639a09343aceabd3a0917dbf078c1aa06
Lokman <muhammad.lokman@hp.com>
2019-06-13 09:34:44 +0800

    added Procfile

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const PORT = 4000;
+const PORT = process.env.PORT || 5000;
 const todoRoutes = express.Router();
 
 let Todo = require('./todo.model');
